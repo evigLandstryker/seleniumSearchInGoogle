5fe27da3a45a85159d1df8129e578bf9a7f7779a
evigLandstryker <92390788+evigLandstryker@users.noreply.github.com>
2021-10-21 23:05:17 +0300

    Refactoring
    Refactoring of last version of code after new lesson

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,11 +10,16 @@ public class Main {
         driver.manage().window().maximize();
         driver.get("https://www.google.com/");
 
-        String xpathInput = "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input";
-        WebElement googleInput = driver.findElement(By.xpath(xpathInput));
+        String xpathSearch = "//input[@type=\"text\"]";
+        WebElement googleInput = driver.findElement(By.xpath(xpathSearch));
         googleInput.sendKeys("softserve it academy\n");
 
-        String urlSoftserve = "//*[@id=\"rso\"]/div[2]/div/div/div[1]/a/div/cite/span";
+        //Кнопка "Поиск" почему-то не получает ключ Enter ни в каком виде
+        /*String xpathButtonSearch = "//input[@type=\"submit\"]";
+        WebElement buttonSearch = driver.findElement(By.xpath(xpathButtonSearch));
+        buttonSearch.sendKeys("ENTER");*/
+
+        String urlSoftserve = "//*[@id=\"rso\"]/div[1]";
         WebElement softserve = driver.findElement(By.xpath(urlSoftserve));
         softserve.click();
 
